0aaaead99531d1448ac1da866810ead24227d26a
Matias Klemola <matias.klemola@gmail.com>
2021-07-08 13:27:25 +0300

    Learn wgpu: step 4: buffers and indices + exercise

--- learn-wgpu/src/main.rs
+++ learn-wgpu/src/main.rs
@@ -1,9 +1,113 @@
+use wgpu::util::DeviceExt;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
     window::Window,
     window::WindowBuilder,
 };
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+impl Vertex {
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::InputStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+            ],
+        }
+    }
+}
+
+const VERTICES: &[Vertex] = &[
+    // A
+    Vertex {
+        position: [-0.0868241, 0.49240386, 0.0],
+        color: [1.0, 0.0, 0.0],
+    },
+    // B
+    Vertex {
+        position: [-0.49513406, 0.06958647, 0.0],
+        color: [1.0, 0.5, 0.0],
+    },
+    // C
+    Vertex {
+        position: [-0.21918549, -0.44939706, 0.0],
+        color: [0.0, 1.0, 0.0],
+    },
+    // D
+    Vertex {
+        position: [0.35966998, -0.3473291, 0.0],
+        color: [0.5, 0.5, 1.0],
+    },
+    // E
+    Vertex {
+        position: [0.44147372, 0.2347359, 0.0],
+        color: [0.0, 0.0, 1.0],
+    },
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 4, // 1
+    1, 2, 4, // 2
+    2, 3, 4, // 3
+    0, // padding
+];
+
+const COMPLEX_VERTICES: &[Vertex] = &[
+    // A
+    Vertex {
+        position: [-0.0868241, 0.49240386, 0.0],
+        color: [1.0, 0.0, 0.0],
+    },
+    // B
+    Vertex {
+        position: [-0.49513406, 0.06958647, 0.0],
+        color: [1.0, 0.5, 0.0],
+    },
+    // C
+    Vertex {
+        position: [-0.21918549, -0.44939706, 0.0],
+        color: [0.0, 1.0, 0.0],
+    },
+    // D
+    Vertex {
+        position: [0.35966998, -0.3473291, 0.0],
+        color: [0.5, 0.5, 1.0],
+    },
+    // E
+    Vertex {
+        position: [0.44147372, 0.2347359, 0.0],
+        color: [0.0, 0.0, 1.0],
+    },
+    // F
+    Vertex {
+        position: [0.7, 0.0, 0.0],
+        color: [1.0, 1.0, 1.0],
+    },
+];
+
+const COMPLEX_INDICES: &[u16] = &[
+    0, 1, 4, // 1
+    1, 2, 4, // 2
+    2, 3, 4, // 3
+    4, 3, 5, // 4
+];
 
 struct State {
     surface: wgpu::Surface,
@@ -13,9 +117,14 @@ struct State {
     swap_chain: wgpu::SwapChain,
     size: winit::dpi::PhysicalSize<u32>,
     render_pipeline: wgpu::RenderPipeline,
-    render_pipeline_alternative: wgpu::RenderPipeline,
-    use_alt_pipeline: bool,
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
+    vertex_buffer_complex: wgpu::Buffer,
+    index_buffer_complex: wgpu::Buffer,
+    num_indices_complex: u32,
     clear_color: wgpu::Color,
+    use_complex_shape: bool,
 }
 
 impl State {
@@ -77,7 +186,7 @@ impl State {
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: "main",
-                buffers: &[],
+                buffers: &[Vertex::desc()],
             },
             fragment: Some(wgpu::FragmentState {
                 module: &shader,
@@ -106,41 +215,33 @@ impl State {
             },
         });
 
-        let render_pipeline_alternative =
-            device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-                label: Some("Render Pipeline"),
-                layout: Some(&render_pipeline_layout),
-                vertex: wgpu::VertexState {
-                    module: &shader,
-                    entry_point: "challenge",
-                    buffers: &[],
-                },
-                fragment: Some(wgpu::FragmentState {
-                    module: &shader,
-                    entry_point: "challenge",
-                    targets: &[wgpu::ColorTargetState {
-                        format: sc_desc.format,
-                        blend: Some(wgpu::BlendState::REPLACE),
-                        write_mask: wgpu::ColorWrite::ALL,
-                    }],
-                }),
-                primitive: wgpu::PrimitiveState {
-                    topology: wgpu::PrimitiveTopology::TriangleList,
-                    strip_index_format: None,
-                    front_face: wgpu::FrontFace::Ccw,
-                    cull_mode: Some(wgpu::Face::Back),
-                    // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODEL
-                    polygon_mode: wgpu::PolygonMode::Fill,
-                    clamp_depth: false,
-                    conservative: false,
-                },
-                depth_stencil: None,
-                multisample: wgpu::MultisampleState {
-                    count: 1,
-                    mask: !0,
-                    alpha_to_coverage_enabled: false,
-                },
-            });
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(VERTICES),
+            usage: wgpu::BufferUsage::VERTEX,
+        });
+
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: wgpu::BufferUsage::INDEX,
+        });
+
+        let num_indices = INDICES.len() as u32;
+
+        let vertex_buffer_complex = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(COMPLEX_VERTICES),
+            usage: wgpu::BufferUsage::VERTEX,
+        });
+
+        let index_buffer_complex = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(COMPLEX_INDICES),
+            usage: wgpu::BufferUsage::INDEX,
+        });
+
+        let num_indices_complex = COMPLEX_INDICES.len() as u32;
 
         let clear_color = wgpu::Color {
             r: 0.0,
@@ -157,9 +258,14 @@ impl State {
             swap_chain,
             size,
             render_pipeline,
-            render_pipeline_alternative,
-            use_alt_pipeline: false,
+            vertex_buffer,
+            index_buffer,
+            num_indices,
+            vertex_buffer_complex,
+            index_buffer_complex,
+            num_indices_complex,
             clear_color,
+            use_complex_shape: false,
         }
     }
 
@@ -178,13 +284,11 @@ impl State {
                     virtual_keycode: Some(VirtualKeyCode::Space),
                     ..
                 } => {
-                    self.use_alt_pipeline = !self.use_alt_pipeline;
-
+                    self.use_complex_shape = !self.use_complex_shape;
                     true
                 }
                 _ => false,
             },
-
             _ => false,
         }
     }
@@ -213,12 +317,26 @@ impl State {
             depth_stencil_attachment: None,
         });
 
-        render_pass.set_pipeline(if self.use_alt_pipeline {
-            &self.render_pipeline_alternative
+        let vertex_buffer = if self.use_complex_shape {
+            &self.vertex_buffer_complex
         } else {
-            &self.render_pipeline
-        });
-        render_pass.draw(0..3, 0..1);
+            &self.vertex_buffer
+        };
+        let index_buffer = if self.use_complex_shape {
+            &self.index_buffer_complex
+        } else {
+            &self.index_buffer
+        };
+        let num_indices = if self.use_complex_shape {
+            self.num_indices_complex
+        } else {
+            self.num_indices
+        };
+
+        render_pass.set_pipeline(&self.render_pipeline);
+        render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
+        render_pass.set_index_buffer(index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+        render_pass.draw_indexed(0..num_indices, 0, 0..1);
 
         // release the encoder mutable borrow
         drop(render_pass);
